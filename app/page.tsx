72f3d5b5bd991bd4bec831026313e5cce8c2adb0
PedroReves <pedroreves11@gmail.com>
2024-07-04 15:37:39 -0300

    Feat: Creation of Main component Files

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Stack from "./components/Stack";
+
 export default function Home() {
-  return <h1>Olá Mundo</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Stack />
+      <Projects />
+      <Footer />
+    </>
+  );
 }
