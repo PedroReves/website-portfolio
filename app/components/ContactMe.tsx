e9b863322ae2073d6332a86b31e283c478af55fa
PedroReves <pedroreves11@gmail.com>
2024-07-04 23:53:58 -0300

    Feat: Add Contact Me Component

---
+++ app/components/ContactMe.tsx
@@ -0,0 +1,12 @@
+export default function ContactMe() {
+  return (
+    <section>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </form>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,19 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Stack from "./components/Stack";
+import ContactMe from "./components/ContactMe";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="bg-bg_black">
-        <Hero />
-        <Stack />
-        <Projects />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main className="bg-bg_black">
+				<Hero />
+				<Stack />
+				<Projects />
+				<ContactMe />
+			</main>
+			<Footer />
+		</>
+	);
 }
