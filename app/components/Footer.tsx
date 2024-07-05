5ac024da3af53ade4f9c02f748a22c62a2bd1d9f
PedroReves <pedroreves11@gmail.com>
2024-07-05 00:46:21 -0300

    Feat: Create Footer Component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,18 @@
+import { FaGithub, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="text-center text-white py-12 bg-bg_black">
+      <h2 className="text-5xl font-bold pb-2.5">Pedro Reves</h2>
+      <p className="text-2xl font-normal pb-5">FullStack Developer</p>
+      <div className="flex justify-center gap-x-6">
+        <span className="bg-[#2C2C2C] p-2 rounded-full cursor-pointer hover:bg-[#212121]">
+          <FaGithub size={25} />
+        </span>
+        <span className="bg-[#2C2C2C] p-2 rounded-full cursor-pointer hover:bg-[#212121]">
+          <FaInstagram size={25} />
+        </span>
+      </div>
+    </footer>
+  );
 }
